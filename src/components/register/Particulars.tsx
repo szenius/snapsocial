49c773b8d091e2a87d8630a5a7095df973ec45a9
Sze Ying <ting.szeying@gmail.com>
2020-06-07 19:44:28 +0800

    feat: register form

--- src/components/register/Particulars.tsx
+++ src/components/register/Particulars.tsx
@@ -1,10 +1,42 @@
 import React, { FunctionComponent } from "react";
-import { Button } from "react-native";
+import { Button, View, StyleSheet, TextInput, Text } from "react-native";
 
 interface IParticularsProps {
+  setHumanName: (input: string) => void;
+  setHumanUrl: (input: string) => void;
   onSubmit: () => void;
 }
 
 export const Particulars: FunctionComponent<IParticularsProps> = ({
+  setHumanName,
+  setHumanUrl,
   onSubmit,
-}) => <Button title="Done" onPress={onSubmit} />;
+}) => (
+  <View style={styles.container}>
+    <View style={styles.inputWrapper}>
+      <Text style={styles.inputLabel}>Your name:</Text>
+      <TextInput style={styles.inputBox} onChangeText={setHumanName} />
+    </View>
+    <View style={styles.inputWrapper}>
+      <Text style={styles.inputLabel}>Your url:</Text>
+      <TextInput style={styles.inputBox} onChangeText={setHumanUrl} />
+    </View>
+    <View style={styles.buttonWrapper}>
+      <Button title="Done" onPress={onSubmit} />
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: 30, alignItems: "center" },
+  inputWrapper: { width: "90%", marginTop: 10, marginBottom: 10 },
+  inputLabel: { fontSize: 15 },
+  inputBox: {
+    borderWidth: 1,
+    height: 40,
+    borderColor: "gray",
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonWrapper: { width: "90%", marginTop: 30, marginBottom: 10 },
+});
--- src/components/register/RegisterScreen.tsx
+++ src/components/register/RegisterScreen.tsx
@@ -3,6 +3,7 @@ import { CameraContainer } from "../CameraContainer";
 import { CameraCapturedPicture } from "expo-camera";
 import { Particulars } from "./Particulars";
 import { RegisterSucess } from "./RegisterSuccess";
+import { IHuman } from "../../context/PopulationContextProvider";
 
 enum RegisterStage {
   Particulars,
@@ -12,19 +13,35 @@ enum RegisterStage {
 
 export const RegisterScreen = () => {
   const [stage, setStage] = useState<RegisterStage>(RegisterStage.Particulars);
-
-  const onPhotoTaken = (photo: CameraCapturedPicture) => {
-    console.log(photo);
-    setStage(RegisterStage.Success);
-  };
+  const [human, setHuman] = useState<IHuman>({
+    name: "",
+    url: "",
+    embedding: [],
+  });
 
   switch (stage) {
     case RegisterStage.Photo:
+      const onPhotoTaken = (photo: CameraCapturedPicture) => {
+        console.log(photo);
+        setStage(RegisterStage.Success);
+      };
       return <CameraContainer onPhotoTaken={onPhotoTaken} />;
     case RegisterStage.Success:
       return <RegisterSucess />;
     case RegisterStage.Particulars:
     default:
-      return <Particulars onSubmit={() => setStage(RegisterStage.Photo)} />;
+      const setHumanName = (input: string) => {
+        setHuman({ ...human, name: input });
+      };
+      const setHumanUrl = (input: string) => {
+        setHuman({ ...human, url: input });
+      };
+      return (
+        <Particulars
+          setHumanName={setHumanName}
+          setHumanUrl={setHumanUrl}
+          onSubmit={() => setStage(RegisterStage.Photo)}
+        />
+      );
   }
 };
